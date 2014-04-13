ac09e644f7a35352c32c5ed04b45c85bcdfb296f
mballin <matt.ballin@gmail.com>
2014-04-13 14:38:11 -0700

    Fixed duplicate #19 + minor proofing

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/ChooseYourOwnAdventure.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/ChooseYourOwnAdventure.java
@@ -7,59 +7,59 @@ public class ChooseYourOwnAdventure
     //   startStory (recipe below) --#2
     //   ------------- Recipe for startStory --#2
     //      Tell the user "One morning the Tortoise woke up in a dream." --#1
-    //      animateStartStory (recipe below) --#38
-    //      ------------- Recipe for animateStartStory --#38
-    //         Show the Tortoise --#37
-    //         The current color is black --#39.2
-    //         Do the following 25 times --#41
-    //         Turn the background to the current color --#39.1
-    //         lighten the current color --#42
-    //         wait for 100 milliseconds --#40  
+    //      animateStartStory (recipe below) --#39
+    //      ------------- Recipe for animateStartStory --#39
+    //         Show the Tortoise --#38
+    //         The current color is black --#40.2
+    //         Do the following 25 times --#42
+    //         Turn the background to the current color --#40.1
+    //         lighten the current color --#43
+    //         wait for 100 milliseconds --#41  
     //         Repeat --#41
-    //      ------------- End of animateStartStory recipe --#38
+    //      ------------- End of animateStartStory recipe --#39
     //      Ask the user "Do you want to 'wake up' or 'explore' the dream?" --#3
     //      If they answer "wake up" --#6
     //      wakeUp (recipe below) --#5
     //      ------------- Recipe for wakeUp --#5
-    //         Tell the user "You Wake up and have a boring day.  The End." --#4
+    //         Tell the user "You wake up and have a boring day.  The End." --#4
     //      ------------- End of wakeUp recipe --#5
     //      Otherwise, if they answer "explore" --#9
     //      approachOoze (recipe below) --#8
     //      ------------- Recipe for approachOoze --#8
-    //         Tell the user "You approach a glowing, green bucket of ooze, worried that you will get in trouble, you pick up the bucket." --#7
+    //         Tell the user "You approach a glowing, green bucket of ooze. Worried that you will get in trouble, you pick up the bucket." --#7
     //         Ask the user "Do you want to pour the ooze into the 'backyard' or 'toilet'?" --#13
     //         If they answer "toilet" --#16
     //         pourIntoToilet (recipe below) --#15
     //         ------------- Recipe for pourIntoToilet --#15
-    //            Tell the user "As you pour the ooze into the toilet it backs up, gurgles and explodes covering you in radio-active waste." --#14
-    //            Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#29
-    //            If they answer "yes" --#32
-    //            ninjaTortoise (recipe below) --#31
-    //            ------------- Recipe for ninjaTortoise --#31
-    //               Tell the user "Awesome Dude!  You live out the rest of your life fighting crimes and eating pizza!" --#30
-    //            ------------- End of ninjaTortoise recipe --#31
-    //            Otherwise, if they answer "heck yes" --#34
-    //            ninjaTortoise (recipe below) --#33
-    //            Otherwise, if they answer anything else --#36
-    //            badAnswer (recipe below) --#35
+    //            Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#14
+    //            Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#30
+    //            If they answer "yes" --#33
+    //            ninjaTortoise (recipe below) --#32
+    //            ------------- Recipe for ninjaTortoise --#32
+    //               Tell the user "Awesome Dude!  You live out the rest of your life fighting crimes and eating pizza!" --#31
+    //            ------------- End of ninjaTortoise recipe --#32
+    //            Otherwise, if they answer "heck yes" --#35
+    //            ninjaTortoise (recipe below) --#34
+    //            Otherwise, if they answer anything else --#37
+    //            badAnswer (recipe below) --#36
     //         ------------- End of pourIntoToilet recipe --#15
     //         Otherwise, if they answer "backyard" --#19
     //         pourIntoBackyard (recipe below) --#18
     //         ------------- Recipe for pourIntoBackyard --#18
-    //            Tell the user "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water." --#17
-    //            Ask the user "As the man starts to prepare you as soup, do you...  'Scream' or 'Faint'?" --#21
-    //            If they answer "faint" --#24
-    //            tortoiseSoup (recipe below) --#23
-    //            ------------- Recipe for tortoiseSoup --#23
-    //               Tell the user "You made a delicious soup!  Yum!  The End." --#22
-    //            ------------- End of tortoiseSoup recipe --#23
-    //            Otherwise, if they answer "scream" --#26
-    //            startStory (recipe below) --#25
-    //            Otherwise, if they answer anything else --#28
-    //         badAnswer (recipe below) --#27
+    //            Tell the user "As you walk into the backyard, a net scoops you up and a giant takes you to a boiling pot of water." --#17
+    //            Ask the user "As the man starts to prepare you as soup, do you...  'Scream' or 'Faint'?" --#22
+    //            If they answer "faint" --#25
+    //            tortoiseSoup (recipe below) --#24
+    //            ------------- Recipe for tortoiseSoup --#24
+    //               Tell the user "You made a delicious soup!  Yum!  The End." --#23
+    //            ------------- End of tortoiseSoup recipe --#24
+    //            Otherwise, if they answer "scream" --#27
+    //            startStory (recipe below) --#26
+    //            Otherwise, if they answer anything else --#29
+    //         badAnswer (recipe below) --#28
     //         ------------- End of pourIntoBackyard recipe --#18
-    //         Otherwise, if they answer anything else --#20
-    //         badAnswer (recipe below) --#19
+    //         Otherwise, if they answer anything else --#21
+    //         badAnswer (recipe below) --#20
     //      ------------- End of approachOoze recipe --#8
     //      Otherwise, if they answer anything else --#12
     //      badAnswer (recipe below) --#11

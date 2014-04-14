0b9c2dd459a5e7fee21505e6e536675141a2dc86
mballin <matt.ballin@gmail.com>
2014-04-13 17:02:54 -0700

    Answer sets for AdLibs and SpiderWeb

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/answerSets/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/answerSets/AdLibs.java
@@ -1,17 +1,18 @@
 package org.teachingkidsprogramming.answerSets;
 
+import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
   public static void main(String[] args)
   {
-    //Ask the user to enter an adverb, save it as currentAdverb --#2
-    //Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
-    //Ask the user to enter a body part, save it as currentBodyPart --#6
-    //Set the value of the currentStory to the word "Today " --#1.2
-    //Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
-    //Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
-    //Add the words "my " + currentBodyPart + ". " to the current story --#7
-    //Show the currentStory in a message box as a message --#1.1
+    String currentAdverb = MessageBox.askForTextInput("Enter an adverb");
+    String currentEdVerb = MessageBox.askForTextInput("Enter a verb ending with -ed");
+    String currentBodyPart = MessageBox.askForTextInput("Enter a body part");
+    String currentStory = "Today ";
+    currentStory = currentStory + "I woke " + currentAdverb + ". ";
+    currentStory = currentStory + "Then I " + currentEdVerb + " ";
+    currentStory += "my " + currentBodyPart + ". ";
+    MessageBox.showMessage(currentStory);
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/answerSets/SpiderWeb.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/answerSets/SpiderWeb.java
@@ -1,31 +1,41 @@
 package org.teachingkidsprogramming.answerSets;
 
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
-    //    Make the tortoise go as fast as possible --#6
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
-    //    Change the Tortoise to a Spider --#14
-    //    Change the color of the line the tortoise draws to silver --#13
-    //    The current length of a line is 10.5 pixels --#1.2                  
-    //    The current zoom is 1.1 --#8.2
-    //    Do the following 10 times --#10
-    //        weaveOneLayer (recipe below) --#9
-    //        ------------- Recipe for weaveOneLayer --#9
-    //        Do the following 6 times --#5
-    //            drawTriangle (recipe below) --#4.2
-    //            ------------- Recipe for drawTriangle --#4
-    //            Do the following 3 times --#3
-    //                Move the tortoise the length of a line --#1.1
-    //                Turn the tortoise 1/3rd of 360 degrees --#2
-    //            Repeat
-    //            ------------- End of drawTriangle recipe --#4
-    //            Turn the tortoise 1/6th of 360 degrees to the right --#7
-    //            Increase the length of the line by the current zoom --#8.1
-    //        Repeat
-    //        ------------- End of weaveOneLayer recipe --#9
-    //        Change the zoom so it is multiplied by 1.3 --#11
-    //    Repeat
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(1);
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(Colors.Grays.Silver);
+    double lineLength = 10.5;
+    double zoom = 1.1;
+    for (int i = 1; i <= 10; i++)
+    {
+      lineLength = weaveOneLayer(lineLength, zoom);
+      zoom = 1.3;
+    }
+  }
+  private static double weaveOneLayer(double lineLength, double zoom)
+  {
+    for (int i = 1; i <= 6; i++)
+    {
+      drawTriangle(lineLength);
+      Tortoise.turn(360 / 6);
+      lineLength += zoom;
+    }
+    return lineLength;
+  }
+  private static void drawTriangle(double lineLength)
+  {
+    for (int i = 1; i <= 3; i++)
+    {
+      Tortoise.move(lineLength);
+      Tortoise.turn(360 / 3);
+    }
   }
 }
